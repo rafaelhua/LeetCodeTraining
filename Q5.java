63681608c969eafdf6729da8dc585044e531a08f
Rafael Hua <huarafael@gmail.com>
2020-09-13 23:12:26 -0300

    CodingInterviews Q5

---
+++ Q5.java
@@ -0,0 +1,28 @@
+/**
+ * 剑指 Offer 05. 替换空格
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * 示例 1：
+ *
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ */
+
+public class Q5 {
+    public String replaceSpace(String s) {
+        int length = s.length();
+        char[] array = new char[length * 3];
+        int size = 0;
+        for (int i= 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                array[size++] = '%';
+                array[size++] = '2';
+                array[size++] = '0';
+            } else {
+                array[size++] = c;
+            }
+        }
+        String newStr = new String(array, 0, size);
+        return newStr;
+    }
+}
